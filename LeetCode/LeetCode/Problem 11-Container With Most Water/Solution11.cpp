87a24889610e791f31d53c8aca327bde7558e55a
musenlao4 <835185523@qq.com>
2018-04-17 23:52:30 +0800

    Solution11
    Complete

---
+++ LeetCode/LeetCode/Problem 11-Container With Most Water/Solution11.cpp
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+#include "Solution11.h"
+
+
+Solution11::Solution11()
+{
+}
+
+
+Solution11::~Solution11()
+{
+}
+
+int Solution11::maxArea(vector<int>& height)
+{
+	map<int, int> mapPoints;
+
+	map<int, int>::iterator it;
+	map<int, int>::iterator itCenter;
+	// 记录多边形各顶点
+	for (int i = 0; i < height.size(); ++i)
+	{
+		// 记录最右值
+		mapPoints[i] = height[i];
+		if (i == 0)
+		{
+			itCenter = mapPoints.end();
+			--itCenter;
+			continue;
+		}
+		// 对齐，去除中间不必要的点
+		it = mapPoints.end();
+		--it;
+		while (it != mapPoints.begin())
+		{
+			--it;
+			if (it->first == itCenter->first)
+				break;
+			if (it->second > height[i])
+				break;
+			it = mapPoints.erase(it);
+		}
+		// 记录最大高度索引
+		if (height[i] > itCenter->second)
+		{
+			itCenter = mapPoints.end();
+			--itCenter;
+		}
+	}
+	// 计算多边形中最大四边形，从最大高度索引开始
+	map<int, int>::iterator itLeft = itCenter;
+	map<int, int>::iterator itRight = itCenter;
+	int nMaxContainer = 0;
+	int nLeft = 0;
+	int nLeftIndex = 0;
+	int nRight = 0;
+	int nRightIndex = 0;
+
+	while (itLeft != mapPoints.begin() || itRight != mapPoints.end())
+	{
+		nLeft = itLeft->second;
+		nLeftIndex = itLeft->first;
+
+		if (itRight != mapPoints.end())
+		{
+			nRight = itRight->second;
+			nRightIndex = itRight->first;
+		}
+		int nHeight = nLeft < nRight ? nLeft : nRight;
+		int nWidth = nRightIndex - nLeftIndex;
+		int nContainer = nHeight * nWidth;
+		nMaxContainer = nMaxContainer > nContainer ? nMaxContainer : nContainer;
+
+		if (itLeft != mapPoints.begin() && itRight != mapPoints.end())
+		{
+			--itLeft;
+			++itRight;
+			if (itLeft->second > itRight->second)
+				--itRight;	// 还原右值
+			else if (itLeft->second < itRight->second)
+				++itLeft;	// 还原左值
+		}
+		else if (itLeft == mapPoints.begin() && itRight != mapPoints.end())
+			++itRight;
+		else if (itLeft != mapPoints.begin() && itRight == mapPoints.end())
+			--itLeft;
+	}
+	return nMaxContainer;
+}
