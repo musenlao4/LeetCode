731a159d7c7a6ea1030811221028462a51cf5b83
jinxin <jinxin@mysnail.com>
2019-12-26 18:32:56 +0800

    Solution 53 improve.

--- LeetCode/LeetCode/Problem 53-Maximum Subarray/Solution53.cpp
+++ LeetCode/LeetCode/Problem 53-Maximum Subarray/Solution53.cpp
@@ -16,25 +16,12 @@ int Solution53::maxSubArray(vector<int>& nums)
 	{
 		return 0;
 	}
-	int max{ INT_MIN };
-	int lastPos{ -1 };
-	for (int i = 0; i < nums.size(); ++i)
+	int max{ nums[0] };
+	int curSum{ nums[0] };
+	for (int i = 1; i < nums.size(); ++i)
 	{
-		int sum{ 0 };
-		for (int j = lastPos + 1; j <= i; ++j)
-		{
-			sum += nums[j];
-		}
-		if (sum > 0)
-		{
-			max = sum > max ? sum : max;
-			max = nums[i] > max ? nums[i] : max;
-		}
-		else
-		{
-			max = nums[i] > max ? nums[i] : max;
-			lastPos = i;
-		}
+		curSum = curSum + nums[i] > nums[i] ? curSum + nums[i] : nums[i];
+		max = curSum > max ? curSum : max;
 	}
 	return max;
 }
