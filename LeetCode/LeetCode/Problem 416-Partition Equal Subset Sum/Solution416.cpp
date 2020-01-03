e71641fc256020ab096f7d0b32c9f6b452189a0a
jinxin <jinxin@mysnail.com>
2020-01-03 13:27:11 +0800

    Solution 416 improve. Dimensionality reduction.

--- LeetCode/LeetCode/Problem 416-Partition Equal Subset Sum/Solution416.cpp
+++ LeetCode/LeetCode/Problem 416-Partition Equal Subset Sum/Solution416.cpp
@@ -22,11 +22,14 @@ bool Solution416::canPartition(vector<int>& nums)
 		return false;
 	}
 	int target{ sum / 2 };
-	vector<vector<int>> dp(nums.size() + 1, vector<int>(target + 1, false));
-	for (int i = 0; i < nums.size() + 1; ++i)
-	{
-		dp[i][0] = true;
-	}
+	//vector<vector<int>> dp(nums.size() + 1, vector<int>(target + 1, false));
+	vector<int> dp(target + 1, false);
+	//for (int i = 0; i < nums.size() + 1; ++i)
+	//{
+	//	dp[i][0] = true;
+	//}
+	dp[0] = true;
+	/*
 	for (int i = 1; i < nums.size() + 1; ++i)
 	{
 		for (int j = 0; j <= target; ++j)
@@ -37,6 +40,21 @@ bool Solution416::canPartition(vector<int>& nums)
 				dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
 			}
 			if (j == target && dp[i][j])
+			{
+				return true;
+			}
+		}
+	}
+	*/
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		for (int v = target; v >= 0; --v)
+		{
+			if (v >= nums[i])
+			{
+				dp[v] = dp[v] || dp[v - nums[i]];
+			}
+			if (v == target && dp[v])
 			{
 				return true;
 			}
