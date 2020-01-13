d6bff9d0551f88b5c6e1af7108ce7b976cb75f90
fooly <835185523@qq.com>
2020-01-13 19:21:01 +0800

    Solution 84 complete.

--- LeetCode/LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode/LeetCode.cpp
@@ -649,7 +649,7 @@ void test82()
 void test84()
 {
 	Solution84 sln;
-	vector<int> heights{ 2,1,5,6,2,3 };
+	vector<int> heights{ 2,1,2 };
 	int res{ sln.largestRectangleArea(heights) };
 	printf("");
 }
--- LeetCode/LeetCode/Problem 84-Largest Rectangle in Histogram/Solution84.cpp
+++ LeetCode/LeetCode/Problem 84-Largest Rectangle in Histogram/Solution84.cpp
@@ -15,24 +15,30 @@ int Solution84::largestRectangleArea(vector<int>& heights)
 	if (heights.empty())
 		return 0;
 	int largest{ 0 };
-	map<int, int> record{};
-	for (auto height : heights)
+	vector<pair<int, int>> stack{};
+	for (int i{ 0 }; i < heights.size(); ++i)
 	{
-		if (record[height] == 1)
-			continue;
-		vector<int> dp(heights.size(), 0);
-		for (int i = 0; i < heights.size(); ++i)
+		int index{ -1 };
+		while (!stack.empty() && stack.back().first > heights[i])
 		{
-			if (heights[i] >= height)
-			{
-				if (i == 0)
-					dp[i] = height;
-				else
-					dp[i] = dp[i - 1] + height;
-				largest = dp[i] > largest ? dp[i] : largest;
-			}
+			int area{ stack.back().first * (i - stack.back().second) };
+			largest = area > largest ? area : largest;
+			index = stack.back().second;
+			stack.pop_back();
 		}
-		record[height] = 1;
+		if (heights[i] != 0)
+		{
+			if (index == -1)
+				stack.emplace_back(pair<int, int>(heights[i], i));
+			else
+				stack.emplace_back(pair<int, int>(heights[i], index));
+		}
+	}
+	while (!stack.empty())
+	{
+		int area{ stack.back().first * (static_cast<int>(heights.size()) - stack.back().second) };
+		largest = area > largest ? area : largest;
+		stack.pop_back();
 	}
 	return largest;
 }
